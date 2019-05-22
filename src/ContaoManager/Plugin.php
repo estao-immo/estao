aa5d56756740186c30fb90f78b95cc2d7d3e90f5
Christian Barkowsky <hallo@christianbarkowsky.de>
2019-05-22 11:25:13 +0200

    #1

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -16,13 +16,12 @@ declare(strict_types=1);
  * @license proprietary
  */
 
-namespace Estao\Info\ContaoManager;
+namespace Estao\InfoBundle\ContaoManager;
 
 use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
-use Estao\Info\EstaoInfoBundle;
 
 /**
  * Class Plugin
